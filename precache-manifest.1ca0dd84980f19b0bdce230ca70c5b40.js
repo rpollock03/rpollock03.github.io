60a47ddf0f01a8de7b26c3622a8a2412a9d00f3b
rpollock03 <49661708+rpollock03@users.noreply.github.com>
2021-01-21 08:30:36 +0000

    Updates

---
+++ precache-manifest.1ca0dd84980f19b0bdce230ca70c5b40.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "031b48122375cb26d273d24d6b9adf8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0f939e848268c42959f",
+    "url": "/static/css/main.e2df8680.chunk.css"
+  },
+  {
+    "revision": "fee2a1d581f700f5a54a",
+    "url": "/static/js/2.4d4f920f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4d4f920f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0f939e848268c42959f",
+    "url": "/static/js/main.8c648c23.chunk.js"
+  },
+  {
+    "revision": "47dff7f7d2288501507d",
+    "url": "/static/js/runtime-main.f5105409.js"
+  },
+  {
+    "revision": "5d8b7f991bbd8df1ba8812254d6c6666",
+    "url": "/static/media/blog_auth.5d8b7f99.png"
+  },
+  {
+    "revision": "87a320bc5cf56b082fceda479652dc4e",
+    "url": "/static/media/calculator.87a320bc.gif"
+  },
+  {
+    "revision": "32a696bfc0961295b411660d89ef590d",
+    "url": "/static/media/facebook.32a696bf.jpg"
+  },
+  {
+    "revision": "192c541fb525789eb998838276f1ca51",
+    "url": "/static/media/flask.192c541f.png"
+  },
+  {
+    "revision": "8c640710015d4fcd3bde8fcadd2eb244",
+    "url": "/static/media/forex.8c640710.gif"
+  },
+  {
+    "revision": "a24ae68028709e229f258701689d25d2",
+    "url": "/static/media/jinja-logo-sidebar.a24ae680.png"
+  },
+  {
+    "revision": "5340df2d87d196240fc8c1c6333ab3bc",
+    "url": "/static/media/mql.5340df2d.png"
+  },
+  {
+    "revision": "34f05125f42f3e0b6406584c3be923ed",
+    "url": "/static/media/profile.34f05125.jpg"
+  },
+  {
+    "revision": "1127ba199aff6d5c864fb094349223f8",
+    "url": "/static/media/react native.1127ba19.png"
+  },
+  {
+    "revision": "9f3260ff2d2c7eba84cea9201189fb61",
+    "url": "/static/media/weather.9f3260ff.gif"
+  }
+]);
